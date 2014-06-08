bfdabe53c0468abc4257503b243c1fd1ecd3248b
hansent <thomas.hansen@gmail.com>
2014-06-08 02:20:23 -0500

    save tour data in local storage

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ var screens = require('./screens');
 var queries = require('./queries');
 
 
-
-
-
-
 function main(){
+
+    //remove any delay between touchstart and click event
     fastclick(document.body);
 
     //router
@@ -22,11 +20,16 @@ function main(){
         log: 'info'
     });
 
+    //tours / user data is stored in loca storage
+    var tourData = JSON.parse(localStorage.getItem("tours") || "[]");
+
+
     //our main view controller
     window.app = new Vue({
         el: '#app',
 
         data: {
+            activeTab: 'home',
             currentScreen: getParameterByName('screen') || 'home',
 
             categories: require('./data/categories.json'),
@@ -40,32 +43,35 @@ function main(){
             userLocaton: null,
             showMap: false,
             
-            myTours: [],
+            myTours: tourData,
             context: {},
         },
 
         ready: function(){
             window.router.init("/"+this.currentScreen);
-
-
             queries.findFeaturedLocations(function(err, places){
                 console.log(places);
                 app.featuredLocations = places;
             });
 
+        },
+
+        methods: {
+            saveTours: function(){
+                localStorage.setItem("tours", JSON.stringify(app.myTours));
+                console.log(localStorage.getItem('tours'));
+            }
         }
+
     });
 
     window.map_reset = true;
 }
 
 
-
-
-
 // Wait for device API libraries to load
 function onDeviceReady() {
-    console.log("=== DEVICE READY =========================")
+    console.log("=== DEVICE READY =========================");
     if (window.StatusBar){
         StatusBar.overlaysWebView(false);
         StatusBar.hide();
@@ -74,17 +80,19 @@ function onDeviceReady() {
 }
 
 
+// ONLY USED TO START ON SPECIFIC SCFEEN WHILE DEVELOPING
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+// if not on an actual device...just run main (to allow testing in chrome)
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
     document.addEventListener("deviceready", onDeviceReady, false);
 } else {
     main(); //this is the browser
 }
 
-
-
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
